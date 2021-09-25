2eac884610b19f8b78d2028971db9ebcc8119de1
tishank <tishank@millenniumitesp.com>
2021-09-25 06:02:59 +0530

    export mount

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,4 +1,15 @@
 import faker from "faker"
 
-const cartText = `<div>You have ${faker.random.number()} items in yout cart</div>`;
-document.querySelector('#dev-carts').innerHTML = cartText;
+const mount = (el) => {
+    const cartText = `<div>You have ${faker.random.number()} items in yout cart</div>`;
+    el.innerHTML = cartText;
+}
+
+if (process.env.NODE_ENV == "development") {
+    const el = document.querySelector("#dev-carts");
+    if (el) {
+        mount(el);
+    }
+}
+
+export { mount };
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount } from 'products/ProductsIndex';
+import { mount as mountCart } from 'cart/CartShow';
 
 console.log('container');
+
+mount(document.querySelector('#my-products'));
+mountCart(document.querySelector('#my-carts'));
